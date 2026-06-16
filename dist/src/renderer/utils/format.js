2e47b4fc721bf4d4253289d264775d9779b97dea
Edward Lawrence <edward@Edwards-MacBook-Pro.local>
2026-06-16 12:53:20 -0600

    Remove dist folder from repo

--- dist/src/renderer/utils/format.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatDate = void 0;
-const formatDate = (value) => new Date(value).toLocaleString();
-exports.formatDate = formatDate;
-//# sourceMappingURL=format.js.map
